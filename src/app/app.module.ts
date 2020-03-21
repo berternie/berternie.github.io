dc136a1cd2831fcb0c450eaf7045c87913101910
Berkay Barlas <fpberkay@gmail.com>
2020-03-21 16:38:53 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/keywords.ts
@@ -0,0 +1,5 @@
+export const KEYWORDS = [
+    {value: 'CREATIVITY'},
+    {value: 'curiosity'},
+    {value: 'Quality'},
+]
